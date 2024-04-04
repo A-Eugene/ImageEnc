f6262a6921b611a6fccc3b9febda04e50ba24b95
A-Eugene <amadeuseugenedirgantara@gmail.com>
2024-04-04 10:21:07 +0700

    first commit

---
+++ src/decrypt.js
@@ -0,0 +1,11 @@
+const aes256 = require('aes256');
+const fs = require('fs');
+
+const PASSWORD = 'abcde123';
+const INPUT_IMAGE = './KK_enc.jpg';
+const OUTPUT_IMAGE = './KK_dec.jpg';
+
+const inputBuffer = fs.readFileSync(INPUT_IMAGE);
+const outputBuffer = aes256.decrypt(PASSWORD, inputBuffer);
+
+fs.writeFileSync(OUTPUT_IMAGE, outputBuffer);
